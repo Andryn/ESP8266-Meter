3e7970d501402dd869943937c13fd457a8888757
Andryn <andrysh@mail.ru>
2015-05-01 00:10:49 +0300

    compile

--- compile.lua
+++ compile.lua
@@ -6,8 +6,8 @@ file.remove("httpd.lc");
 file.remove("sensor.lc");
 
 node.compile('config.lua');
-node.compile('GPIO.lua');
-node.compile('wifi.lua');
+node.compile('GPIO_lib.lua');
+node.compile('wifi_lib.lua');
 node.compile('dht_lib.lua');
 node.compile('httpd.lua');
 node.compile('sensor.lua');
